004bafaf6724c6be57a2f2f8f8b879cc0eb9b755
ybKim <ybkim190831@gmail.com>
2022-02-11 17:54:21 +0900

    todo complate!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./ToDoList";
+import ToDoList from "./components/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400&family=Source+Sans+Pro:ital,wght@1,300;1,400&display=swap');
